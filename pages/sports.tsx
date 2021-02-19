b1c64229d5466f3714780f5aad60931665a9effe
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-02-19 12:04:12 +0100

    add moment

--- pages/sports.tsx
+++ pages/sports.tsx
@@ -1,8 +1,9 @@
 import _ from "lodash";
+import moment from "moment";
 
 const Sports = () => {
   const fakeData = _.fill(Array(100), { data: "data" });
-
+  moment();
   return (
     <section>
       <p>Sports</p>
