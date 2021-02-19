7d84445316cde9fc6e0bd05fb8c5bc44ad855dac
Gabriel <gabriel.bernal@joyn.de>
2021-02-19 10:52:30 +0100

    remove series test data

--- pages/series/index.tsx
+++ pages/series/index.tsx
@@ -1,14 +1,7 @@
-import _ from "lodash";
-
-const dummyData = new Array(100).fill({ test: "test" });
-
 const Series = () => {
   return (
     <section>
       <p>Series</p>
-      <ul>
-        <li>{_.chunk(dummyData, 3)}</li>
-      </ul>
     </section>
   );
 };
